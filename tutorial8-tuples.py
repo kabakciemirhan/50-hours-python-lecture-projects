60de7a1aa86919eacca195d76d006a94c15d2410
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2023-12-13 16:35:10 +0300

    tuples-giris

---
+++ tutorial8-tuples.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 16:30:03 2023
+
+@author: emirhankabakci
+"""
+
+#tuples, kapsayıcıdır, sıralıdır fakat değiştirilemez.
+t = ("ali", "veli", 1, 2.6, [2,3,5])
+print(type(t))
+print(len(t))
+
+print(t[2])
