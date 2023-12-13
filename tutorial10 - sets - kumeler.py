9a621de944dc221e73564c76453f894bab824f5a
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2023-12-13 17:55:09 +0300

    Create tutorial10 - sets - kumeler.py

---
+++ tutorial10 - sets - kumeler.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 17:50:02 2023
+
+@author: emirhankabakci
+"""
+
+#setler
+#sırasızdır, değerleri eşsizdir, değiştirilebilirdir, farklı tipleri barındırabilir
+list = [1, "a", "ali", 123]
+s = set(list)
+print(s)
