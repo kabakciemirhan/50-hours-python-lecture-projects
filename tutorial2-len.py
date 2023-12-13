83655da58da2d03e40c75ace12971b36e5995dba
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2023-12-13 12:59:13 +0300

    create t2_len.py / len method
    len uzunluk olcer

---
+++ tutorial2-len.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 12:53:57 2023
+
+@author: emirhankabakci
+"""
+
+#string metodları - len
+
+mvk = "gelecegi_yazanlar"
+
+a = 9
+b = 10
+
+t = a*b
+print(mvk, " ", t)
+
+#len kullanıyoruz
+print(len(mvk))
