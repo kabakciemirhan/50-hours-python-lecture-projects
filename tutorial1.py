35acbc0ac5adae8308d403fb0fbb7038aff69947
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2023-12-13 12:53:51 +0300

    numbers-strings-float-int

--- tutorial1.py
+++ tutorial1.py
@@ -6,3 +6,26 @@ Created on Wed Dec 13 12:40:46 2023
 @author: emirhankabakci
 """
 
+#sayılar ve strings
+9 
+9.2
+9+9
+9*9
+
+print('hello ai era')
+"hello ai era"
+
+#sayının tipini gösterme
+type(9)
+type(9.2)
+type("what is the type of this?")
+
+#deneme yazdırma
+
+x = 9
+y = 9*8
+t = type(9)
+p = type("string")
+print(x,y,t,p)
+
+
