a16be993a5198471e581ca004b04fc2a7f4728d5
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2023-12-13 13:15:51 +0300

    tutorial 5 - substrings
    substrings, diziler ile aynı mantık

---
+++ tutorial5 - substrings.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 13:12:30 2023
+
+@author: emirhankabakci
+"""
+
+#substrings, diziler ile aynı mantık
+strSec = "python_kursu"
+print(strSec[0])
+print(strSec[3])
+#aralığı yazdırmak istiyorsak excel gibi düşünelim
+print(strSec[2:5])
